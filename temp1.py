c5a76cc0d491c58439769e12af33daf334d7ee90
RobertoRomeroHernandez <31378324+RobertoRomeroHernandez@users.noreply.github.com>
2017-09-04 13:30:08 -0500

    Programa modificado

--- temp1.py
+++ temp1.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pylab as pl
-#Crea un  vector (arreglo)con los valores del eje X
-x = [1,2,3,4,5]
-#Crea un  vector (arreglo)con los valores del eje y para cada valor del eje X
-y = [1,2,3,4,5]
-#Grafia el vector x contra el vector y
+x = [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+y = [   0,   0,   0,   1,   1,   1,   1,   2,   2,   2,   1,   1,   0,   0,   0,   2,   1,   0,   0,   0,   1,   1,   2]
+pl.ylabel('Numero de mascotas y novias')
+pl.xlabel('Anio')
+pl.axis([1995,2017,0,21])
 pl.plot (x,y)
-#Guarda la grafica en formato png
 pl.savefig('temp1.png')
+pl.show()
